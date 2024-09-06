21cdb3a744a497c8dce4352babd8a8f7f91da1fe
Abhay Krishnan M N(UST <abhaykrishnan333@gmail.com>
2024-09-06 10:42:10 +0530

    first commit

---
+++ src/test/java/com/automation/pages/HomePage.java
@@ -0,0 +1,6 @@
+package com.automation.pages;
+
+public class HomePage extends BasePage{
+    public void openWebsite() {
+    }
+}
---
+++ src/test/java/com/automation/test/HomePageTest.java
@@ -0,0 +1,13 @@
+package com.automation.test;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest extends BaseTest {
+
+
+    @Test
+    void openWebsite() {
+        homePage.openWebsite();
+    }
+
+}
